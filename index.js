f6d88a2c49b776a594733c270c758e78661aab12
Aleem Said <aleemsaid1100@gmail.com>
2025-07-24 00:15:44 +0500

    Initial push - Day 1

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log(`hai come on`);
+window.alert(`Disclaimer  Disclaimer!!!`);
+alert(`They got in your router `);
+//document.getElementById(`first`).textContent= `Noir Notion`;
+//document.getElementById(`second`).textContent= `OOPs!`;
+let x;
+x=100;   //numbers
+console.log(`x`);
+console.log(x);
+let age;
+age=25;     
+let price=500;
+let name=`aleem`;  //string
+let favouritefood="Rosh";
+let email="aleemsaid1100@gmail.com";
+console.log(`your email is:${email}`);
+console.log(`my favourite food is:${favouritefood}`);
+console.log(typeof name);
+console.log(name);
+console.log(`my name  is:${name}`);
+console.log(`you are ${age} years old`);
+console.log(`ALeem bought this laptop over a price of ${price} $`);
+console.log(`They are playing guessing number game;Zaroon guess over the ${x} number`);
+console.log(typeof price);
+console.log(typeof age);
+console.log(typeof x);
+
+console.log(typeof email);
+let online=true;
+console.log(typeof online);
+console.log(`Aleem is online:${online}`);
+ 
+let supername="Syed Aleem";
+let  bestage=20;
+let isstudent=true;
+document.getElementById(`p1`).textContent= `You are ${supername}`;
+document.getElementById(`p2`).textContent= `Your's age is ${bestage}`;
+document.getElementById(`p3`).textContent= `Enrolled in University:${isstudent}`;
