d1cdc386b7273412a6fab2a5c78197911988d100
Aleem Said <aleemsaid1100@gmail.com>
2025-07-24 23:31:28 +0500

    Added day-2 folder with new responsive registration form

---
+++ day-2/index.js
@@ -0,0 +1,53 @@
+//let students=31;
+//Arithmetic  operations 
+//students=30+2;
+//console.log(`students are:${students}`);
+//console.log(`students are:${students+60}`);
+//students=10-9;
+//console.log(`students:${students}`)
+//students=32*2;
+//console.log(`students are:${students}`);
+//students=32**2;
+//console.log(`students are:${students}`);
+//students=students/2;
+//console.log(`studets division  is:${students}`);
+//students=students%2;
+//console.log(`studetns remindr is:${students}`);
+//Augmented assignmentway
+//students+=4;
+//students-=30;
+//students*=3;
+//students/=2;
+//students%=2;
+//students**=2;
+//increment  and decrement operators
+//students--;
+//students++;
+//BodMas  rules
+//students=1+3-5+2*4+4**2+10/2+(3+2);
+//console.log(`more students  got in:${students}`);
+//let  username;
+//username=window.prompt("Enter your name");
+//let address;
+//address=window.prompt(`enter your address`);
+//console.log(`your user  name  is:${username}`);
+//console.log(`your address is:${address}`);
+//let username;
+//let address;
+//let dob;
+//document.getElementById(`acha`).onclick=function(){
+  //  username= document.getElementById(`person`).value;
+    //address=  document.getElementById(`address`).value;
+    //dob= document.getElementById(`birth`).value;
+   // console.log(username);
+  //  console.log(address);
+//    console.log(dob)
+  //  document.getElementById(`sir`).textContent=`Hello${username}`;
+//}
+
+
+
+
+
+
+
