ec1ced5e1327de59a4bad47c9aba150e0d45b033
Aleem Said <aleemsaid1100@gmail.com>
2025-08-05 23:20:57 +0500

    Added day-8 folder

---
+++ day-8/index.js
@@ -0,0 +1,120 @@
+let height;
+console.log(height);
+
+
+
+//let age;
+//age=130;
+//age>=18 ? console.log(`You are  an audlt`):  console.log(`You are not an adult`);
+//let response;
+//response=age>=18 ? `You are  an  adult`: `You are  not an adult`;
+//console.log(response);
+//let time=12;
+//let hello;
+//hello=time>=12 ? `Good After  Noon` :    `Good  Morning`;
+//console.log(hello);
+//let student=false;
+//let response;
+//response=student ? `You  are a student` : `You are not a student`;
+//console.log(response);
+// purchaseamount=120;
+//let  discount;
+//discount= purchaseamount>=100 ? 10 : 0;
+//let  Ad=purchaseamount *  (discount/100);
+//console.log(`the discount you got  is  $${Ad}`)
+//console.log(`THe  total  price  is $${purchaseamount-Ad}`)
+//let  buying=210;                //dicount
+//let discount;
+//discount= buying>=100 ? 5 :0;             
+//let Discount=buying*discount/100;
+//console.log(`You got discount of: $${Discount}` )
+//let Finalprice=buying-Discount;
+//console.log(`Total  price is:${Finalprice}`);
+//let day=1;
+//switch(day){
+  //  case 1:
+    //    console.log(`It  is Monday`);          //for direct comaprison
+     //   break;
+//    case 2:
+//        console.log(`It  is Tuesday`);
+//        break;
+//    case 3:
+//        console.log(`It  is Wednesday`);
+//        break;
+//    case 4:
+//        console.log(`It  is  Thursday`);
+//        break;
+//    case 5:
+//        console.log(`IT  is  Friday`);
+//        break;
+//    case 6:
+//        console.log(`It is  is Saturday`);
+//        break;
+//    case 7:
+//        console.log(`It  is Sunday`);
+//        break;
+//        default:
+//            console.log(`${day} is not  a day`);
+//}
+//let gender=male;
+//let testing;
+//switch(true){
+//    case gender==`male`:
+//        testing=`Male`;        // for multiple  conditons  
+//        break;
+//    case gender==`female`:
+//        testing=`Female`;
+//        break;
+//    default:
+//        console.log(`${gender} is not a gender`);
+//}
+//console.log(testing);
+//let testScore=92;
+//let  grade;
+//switch(testScore){
+//    case testScore>=90:
+//        grade=`A`;
+//        break;
+//}
+//console.log(grade);
+//switch(true){
+//    case testScore>=90:
+//        grade=`A`;
+//        break;
+//}
+//console.log(grade);
+//let fruit=`apple`;
+//switch(fruit){                           //Direct comparison
+//    case `apple`:
+//        console.log(`apple is selected`);
+//        break;
+//}
+let name=`Aleem`;
+let get;
+//get=name.charAt(1);
+//get=name.indexOf(`A`);
+//get=name.length;
+//get=name.lastIndexOf(`e`);
+//get.name.trim();
+//get=name.toUpperCase();
+//get=name.toLowerCase();
+//console.log(get);
+//console.log(name.toUpperCase());
+//console.log(name.repeat(3));
+//let  result=name.startsWith(" ");
+//console.log(result);
+//let number=`123-544-786`;
+//number=number.replaceAll(`-`,``);
+//number=number.padStart(20,`+`);
+//number=number.padEnd(15, `0`);
+//console.log(number);
+
+//console.log(name.indexof(`A`));
+//let $l=`a`;
+//console.log($l);
+
+
+
+
+
+
